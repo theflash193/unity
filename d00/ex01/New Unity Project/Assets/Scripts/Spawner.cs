0ec1f69f771f26d917ce8db667417086ddd2350e
Gordon RASS-KWASI <grass-kw@e2r7p1.42.fr>
2017-07-19 16:12:25 +0200

    ex01 en cours

---
+++ d00/ex01/New Unity Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	// un objet du type que l'on souhait faire spawn
+	// Use this for initialization
+	public int type;
+	public GameObject a;
+	public GameObject s;
+	public GameObject d;
+
+	void Start () {
+		Vector3 old = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, 0);
+		switch (type) {
+		case 1:
+			GameObject.Instantiate (a, old, Quaternion.identity);
+			break ;
+		case 2:
+			GameObject.Instantiate (s, old, Quaternion.identity);
+			break ;
+		case 3:
+			GameObject.Instantiate (d, old, Quaternion.identity);
+			break;
+		default :
+			break;
+		}
+		Debug.Log (gameObject.transform.localPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
