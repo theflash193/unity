9d000a3e2936f7b4848ef354663060f4ba04e619
Gordon RASS-KWASI <grass-kw@e2r2p8.42.fr>
2017-07-21 09:00:45 +0200

    jeu terminer

---
+++ d00/ex01/New Unity Project/Assets/Scripts/Cube.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	float speed;
+	float timer = 0;
+	float lifetime = 80;
+	public string key;
+	int type;
+	Vector2 posLine;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(0.09f, 0.23f);
+
+		switch (key) {
+		case "s":
+			type = 1;
+			break;
+		case "a":
+			type = 2;
+			break;
+		case "d":
+			type = 3;
+			break;
+		default: 
+			break;
+		}
+		posLine = new Vector2 (0, -3);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timer >= lifetime) {
+			timer = 0;
+			GameObject.Destroy (gameObject);
+		} else {
+			gameObject.transform.Translate (new Vector3 (0, -1, 0) * speed);
+			if (type == 1 && Input.GetButtonDown ("S"))
+				ShowDistanceMessage ();
+			if (type == 2 && Input.GetButtonDown("A"))
+				ShowDistanceMessage ();
+			if (type == 3 && Input.GetButtonDown("D"))
+				ShowDistanceMessage ();
+		}
+		timer += 1;
+
+	}
+
+	void ShowDistanceMessage() {
+		float distance = Vector2.Distance(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), posLine);
+		Debug.Log ("Precision " + distance);
+	}
+}
---
+++ d00/ex01/New Unity Project/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+	float spawnTime = 60f;
+	float timer = 0;
+	int note;
+	public GameObject a;
+	public GameObject s;
+	public GameObject d;
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timer >= spawnTime) {
+			timer = 0;
+			note = Random.Range (1, 4);
+			Debug.Log (note);
+			if (note == 1) {
+				Instantiate (a, new Vector3 (-2.22f, 3, 0), Quaternion.identity);
+			} else if (note == 2) {
+				Instantiate (s, new Vector3 (0, 3, 0), Quaternion.identity);
+			} else {
+				Instantiate (d, new Vector3 (2.22f, 3, 0), Quaternion.identity);
+			}
+		}
+		timer += 1;
+	}
+}
